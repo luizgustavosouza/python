6494693b0a6fcdf94cbf33cea465d67011c6b6bb
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-07-27 23:44:56 -0300

    Exercícios

---
+++ 1015.py
@@ -0,0 +1,21 @@
+#Distancia  Euclidiana entre dois pontos
+import math
+
+from math import sqrt
+
+coord1=input().split()
+
+x1=float(coord1[0])
+y1=float(coord1[1])
+
+coord2=input().split()
+
+x2=float(coord2[0])
+y2=float(coord2[1])
+
+coord1 = (x2-x1)
+coord2 = (y2-y1)
+
+distancia= sqrt(coord1+coord2)
+
+print("%2.4f" %distancia)
