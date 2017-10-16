dcf9c79cb9e0db736beae91042a85133bc4d02c6
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-16 19:20:04 -0200

    Aula 8, exemplo1

---
+++ aula8_1.py
@@ -0,0 +1,19 @@
+#Funções
+
+def listaN(elementos,qtd,min,max):
+    #importando módulo para gerar valores aleatórios
+    from random import randint
+    for i in range(qtd):
+        elementos.append(randint(min,max))
+    return None
+
+# Programa Principal
+
+qtdNumeros = int(input("Qauantos valores devem conter na lista ? "))
+minimo = int(input("Informe o menor valor da faixa: "))
+maximo = int(input("Informe o maior valor da faixa: "))
+
+numeros = []
+
+listaN(numeros,qtdNumeros,minimo,maximo)
+print(numeros)
