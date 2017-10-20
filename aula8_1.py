0931e7e4c761a6c9dd5c9f056b414ef447367838
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-20 19:44:50 -0200

    Implementando funções para ordenação da lista e remoção de elementos repetidos.

--- aula8_1.py
+++ aula8_1.py
@@ -1,20 +1,39 @@
-#Funções
-
+#Subprogramas
 def listaN(elementos,qtd,min,max):
-    #importando módulo para gerar valores aleatórios
-    from random import randint
+    from random import randint #importando módulo para gerar valores aleatórios
     for i in range(qtd):
-        elementos.append(randint(min,max))
+        elementos.append(randint(min,max)) #gerando uma lista com valores aleatórios usando a função randint e append
     return None
 
-# Programa Principal
+def ordenaL(elementos):
+    ordenada = elementos.sort() #ordenando a lista em ordem crescente
+    return None
 
-#Obtendo valores
-qtdNumeros = int(input("Qauantos valores devem conter na lista ? "))
-minimo = int(input("Informe o menor valor da faixa: "))
-maximo = int(input("Informe o maior valor da faixa: "))
+#Removendo os elementos repetidos
+def repetidosL(elementos):
+    i = 0
+    while i<len(elementos):
+        if elementos.count(elementos[i])==1: #Se o elemento só aparecer uma vez contador é incrementado passando para o proximo elemento da lista
+            i+=1
+        else:
+            elementos.remove(elementos[i]) #Se o elemento aparecer mais de uma vez a primeira ocorrencia da esq para a dir do elemento é removida
+    return None
 
-numeros = []
+#Programa Principal
+#Obtendo os valores
+qtd=int(input('Informe a quantidade de elementos na lista: '))
+mini=int(input('Informe o menor valor da faixa: '))
+maxi=int(input('Informe o maior valor da faixa: '))
+elementos=[] #criando uma lista vazia
 
-listaN(numeros,qtdNumeros,minimo,maximo)
-print(numeros)
+listaN(elementos,qtd,mini,maxi) #chamada para a função/módulo/subprograma
+
+print(elementos) #imprimindo a lista
+
+print("Ordenando a lista...")
+ordenaL(elementos) #chamada para a função/módulo/subprograma
+print(elementos)
+
+print("Removendo elementos repetidos...")
+repetidosL(elementos) #chamada para a função/módulo/subprograma
+print(elementos)
