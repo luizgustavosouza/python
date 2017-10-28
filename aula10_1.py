babfdd85cb35c4f341145f52d1c591073074573a
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-28 11:54:17 -0200

    Obtendo a quantidade de integrantes do time

---
+++ aula10_1.py
@@ -0,0 +1,41 @@
+qtdInfraDC=int(input("Digite a quantidade de pessoas no time de Infra DataCenter: "))
+qtdInfraDBA=int(input("Digite a quantidade de pessoas no time de Infra DBA: "))
+qtdInfraNTW=int(input("Digite a quantidade de pessoas no time de Infra Network: "))
+qtdInfraMGM=int(input("Digite a quantidade de pessoas no time de Infra Management: "))
+
+infraDC=set()
+infraDBA=set()
+infraNTW=set()
+infraMGM=set()
+
+gestor="Ygor Moretti"
+
+for i in range(qtdInfraDC):
+    nome=input("Digite o nome do analista de data center: ")
+    infraDC.add(nome)
+
+
+for j in range(qtdInfraDBA):
+    nome=input("Digite o nome do analista de banco de dados: ")
+    infraDBA.add(nome)
+
+
+for k in range(qtdInfraNTW):
+    nome=input("Digite o nome do analista de Redes: ")
+    infraNTW.add(nome)
+
+for l in range(qtdInfraMGM):
+    nome=input("Digite o nome do analista de Gestão: ")
+    infraMGM.add(nome)
+
+print("O time de Infra Data Center é composto por: ", infraDC)
+print()
+print("O time de Infra Data Base é composto por: ", infraDBA)
+print()
+print("O time de Infra Data Base é composto por: ", infraNTW)
+print()
+print("O time de Infra Data Base é composto por: ", infraMGM)
+print()
+operacao = infraDC.union(infraDBA).union(infraNTW).union(infraMGM)
+print()
+print("O time de Operação é composto por: ", operacao)
--- aula11_1.py
+++ aula11_1.py
@@ -1,18 +1,18 @@
 #Criando Dicionários chave:valor
 
 time=dict()  #também pode ser feito time={}
-qtdIntegrantes=int(input())
+qtdIntegrantes=int(input("Digite a quantidade de integrantes no time:  "))
 for i in range(4):
     nome=input("Digite o nome do intgrante: ")
     especialidade=input("Digite a especialidade do integrante: ")
     time[nome]=especialidade
-
 #print(time)
-
 for chave,valor in time.items():
     print(chave,":",valor)
-
 print()
 #Com valores aleatórios
 for chave in sorted(time):
+    print(chave,":",time[chave])
+print()
+for chave in sorted(time.keys()):
     print(chave,":",time[chave])
