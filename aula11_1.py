de9a51887c4d5cbd70bcd2dea87f7fec94e04fad
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-28 12:02:07 -0200

    Imprimindo o dicionário em ordem Alfabética

--- aula11_1.py
+++ aula11_1.py
@@ -4,15 +4,12 @@ time=dict()  #também pode ser feito time={}
 qtdIntegrantes=int(input("Digite a quantidade de integrantes no time:  "))
 for i in range(4):
     nome=input("Digite o nome do intgrante: ")
-    especialidade=input("Digite a especialidade do integrante: ")
+    especialidade=input("Digite a especialidade do "+nome+": " )
     time[nome]=especialidade
 #print(time)
 for chave,valor in time.items():
     print(chave,":",valor)
 print()
-#Com valores aleatórios
+#Com valores em ordem alfabética
 for chave in sorted(time):
-    print(chave,":",time[chave])
-print()
-for chave in sorted(time.keys()):
     print(chave,":",time[chave])
