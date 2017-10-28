6eeb73c544cd8cbd5b229d07556d3f6eeffd5fda
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-28 12:21:20 -0200

    Imprimindo apenas chaves, ordenando chaves e imprimindo apenas os valores

--- aula11_1.py
+++ aula11_1.py
@@ -2,14 +2,21 @@
 
 time=dict()  #também pode ser feito time={}
 qtdIntegrantes=int(input("Digite a quantidade de integrantes no time:  "))
+nomeTime="Infra Data Center"
 for i in range(qtdIntegrantes):
     nome=input("Digite o nome do intgrante: ")
     especialidade=input("Digite a especialidade do "+nome+": " )
     time[nome]=especialidade
-#print(time)
-for chave,valor in time.items():
-    print(chave,":",valor)
 print()
+print("O time "+nomeTime+ " é composto por: ")
+for chave,valor in time.items():
+    print(chave)
+print()
+print("Ordenando...")
 #Com valores em ordem alfabética
 for chave in sorted(time):
-    print(chave,":",time[chave])
+    print(chave)
+print()
+print("As especialidades do time "+nomeTime+" são:")
+for valor in time.values():
+    print(valor)
