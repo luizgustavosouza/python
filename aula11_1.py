8cd220f0b19ecb18ae37972fe7ee49f00fc1ce89
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-28 11:34:26 -0200

    Gerando um Dicionário para um time com os integrantes e suas especialidades

---
+++ aula11_1.py
@@ -0,0 +1,14 @@
+#Criando Dicionários chave:valor
+
+time=dict()  #também pode ser feito time={}
+qtdIntegrantes=int(input())
+for i in range(4):
+    nome=input("Digite o nome do intgrante: ")
+    especialidade=input("Digite a especialidade do integrante: ")
+    time[nome]=especialidade
+
+#print(time)
+
+for chave,valor in time.items():
+    print(chave,":",valor)
+
