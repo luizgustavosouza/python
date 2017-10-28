f4a72e763e1cb482a83b6ebc41a5344a23e6e3ec
luizgustavosouza <luizgustavosouza@id.uff.br>
2017-10-28 12:40:28 -0200

    Ordenando as Especialidades

--- aula11_1.py
+++ aula11_1.py
@@ -3,12 +3,13 @@
 time=dict()  #também pode ser feito time={}
 qtdIntegrantes=int(input("Digite a quantidade de integrantes no time:  "))
 nomeTime="Infra Data Center"
+gestorName=input("Informe o nome do Gestor: ")
 for i in range(qtdIntegrantes):
     nome=input("Digite o nome do intgrante: ")
     especialidade=input("Digite a especialidade do "+nome+": " )
     time[nome]=especialidade
 print()
-print("O time "+nomeTime+ " é composto por: ")
+print("O time "+nomeTime+ " gerenciado por "+gestorName+" é composto por: ")
 for chave,valor in time.items():
     print(chave)
 print()
@@ -19,4 +20,8 @@ for chave in sorted(time):
 print()
 print("As especialidades do time "+nomeTime+" são:")
 for valor in time.values():
+    print(valor)
+print()
+print("Ordenando as especialidades do time "+nomeTime+" :")
+for valor in sorted(time.values()):
     print(valor)
